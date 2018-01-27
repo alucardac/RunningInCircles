4a22c0dc70072f69e77b112c69705836f6ce0865
Michael Consoli <michael@cartwheelgames.com>
2018-01-27 18:37:51 -0500

    Changes to game

--- Assets/Scripts/TileSet/FullTileSet.cs
+++ Assets/Scripts/TileSet/FullTileSet.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class TileSet : ScriptableObject {
+public class PartialTileSet : TileSetBase {
 	[SerializeField]
 	private Sprite empty = null;
 	[SerializeField]
---
+++ Assets/Scripts/TileSet/TileSetBase.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class TileSetBase : ScriptableObject {
+
+	public Sprite GetCase(int caseIndex)
+	{
+		return null;
+	}
+}
