74b6f03deeb20f997806fbed9f4fd1dd5f56a875
Michael Consoli <michael@cartwheelgames.com>
2018-01-27 15:31:17 -0500

    Initial Commit

---
+++ Assets/Scripts/SmallTileSet.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FullTileSet : ScriptableObject {
+	[SerializeField]
+	private Sprite empty = null;
+	[SerializeField]
+	private Sprite full = null;
+	[SerializeField]
+	private Sprite wall1 = null;
+	[SerializeField]
+	private Sprite wall2 = null;
+	[SerializeField]
+	private Sprite wall3 = null;
+	[SerializeField]
+	private Sprite wall4 = null;
+	[SerializeField]
+	private Sprite innerCorner1 = null;
+	[SerializeField]
+	private Sprite innerCorner2 = null;
+	[SerializeField]
+	private Sprite innerCorner3 = null;
+	[SerializeField]
+	private Sprite innerCorner4 = null;
+	[SerializeField]
+	private Sprite corner1 = null;
+	[SerializeField]
+	private Sprite corner2 = null;
+	[SerializeField]
+	private Sprite corner3 = null;
+	[SerializeField]
+	private Sprite corner4 = null;
+	[SerializeField]
+	private Sprite diagonal1 = null;
+	[SerializeField]
+	private Sprite diagonal2 = null;
+	public Sprite GetCase(int caseIndex)
+	{
+		switch (caseIndex)
+		{
+			default:
+				return full;
+			case 1:
+				return innerCorner1;
+			case 2:
+				return innerCorner2;
+			case 3:
+				return innerCorner3;
+			case 4:
+				return innerCorner4;
+			case 5:
+				return wall1;
+			case 6:
+				return wall2;
+			case 7:
+				return wall3;
+			case 8:
+				return wall4;
+			case 9:
+				return diagonal1;
+			case 10:
+				return diagonal2;
+			case 11:
+				return corner1;
+			case 12:
+				return corner2;
+			case 13:
+				return corner3;
+			case 14:
+				return corner4;
+			case 15:
+				return empty;
+		}
+	}
+}
---
+++ Assets/Scripts/TileSet.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class TileSet : ScriptableObject {
+	[SerializeField]
+	private Sprite empty = null;
+	[SerializeField]
+	private Sprite full = null;
+	[SerializeField]
+	private Sprite wall = null;
+	[SerializeField]
+	private Sprite innerCorner = null;
+	[SerializeField]
+	private Sprite corner = null;
+	[SerializeField]
+	private Sprite diagonal = null;
+	public Sprite GetCase(int caseIndex)
+	{
+		switch (caseIndex)
+		{
+			default:
+				return full;
+			case 1:
+			case 2:
+			case 3:
+			case 4:
+				return innerCorner;
+			case 5:
+			case 6:
+			case 7:
+			case 8:
+				return wall;
+			case 9:
+			case 10:
+				return diagonal;
+			case 11:
+			case 12:
+			case 13:
+			case 14:
+				return corner;
+			case 15:
+				return empty;
+		}
+	}
+}
